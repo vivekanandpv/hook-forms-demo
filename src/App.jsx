faf3b5eace8e2a7c7ca48b68236c65346646348b
vivek <vivekanandpv@outlook.com>
2023-07-24 14:55:46 +0530

    starter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Login';
+
+const App = (props) => {
+  return (
+    <>
+      <div className='p-5'>
+        <h2>React Hook Form</h2>
+        <hr />
+        <Login />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = (props) => {
+  return (
+    <>
+      <h3>Login Form</h3>
+    </>
+  );
+};
+
+export default Login;
